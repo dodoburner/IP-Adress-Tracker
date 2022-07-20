ce9780eb7f01fc02a826da5b3720beed8e916715
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-07-20 12:43:28 +0200

    listen form form submit and locate the ip adress

---
+++ src/locate.js
@@ -0,0 +1,16 @@
+const ip = document.querySelector('.ip')
+const location = document.querySelector('.location')
+const timezone = document.querySelector('.timezone')
+const isp = document.querySelector('.isp')
+
+const locate = async (adress) => {
+  let response = await fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_vaX1O20m0nZWgzFhUoJNWRUb5YDdt&ipAddress=${adress}`)
+  let data = await response.json()
+  console.log(data)
+  ip.innerHTML = data.ip
+  location.innerHTML = `${data.location.region} ${data.location.country}`
+  timezone.innerHTML = `UTC ${data.location.timezone}`
+  isp.innerHTML = data.isp
+}
+
+export default locate
