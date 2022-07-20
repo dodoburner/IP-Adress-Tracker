0053743fa40f4c56d7bc8ad72837bed85ecf3a85
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-07-20 15:31:06 +0200

    update styling details

--- src/leaflet.js
+++ src/leaflet.js
@@ -7,10 +7,10 @@ const createMap = (lat, long) => {
   let map = L.map('map').setView([lat, long], 10);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 20,
+      maxZoom: 100,
       attribution: '© OpenStreetMap'
   }).addTo(map);
-  
+
   L.marker([lat, long], {icon: myIcon}).addTo(map);
 }
 
